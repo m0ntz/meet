dec11babd4fe60c9c6c523c2105254a75c305901
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-12 23:53:34 +0100

    re-installed node and fixed issues

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,6 +4,7 @@ class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
+    showSuggestions: undefined,
   };
 
   handleInputChanged = (event) => {
@@ -20,6 +21,7 @@ class CitySearch extends Component {
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      showSuggestions: false,
     });
 
     this.props.updateEvents(suggestion);
@@ -33,8 +35,14 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
         />
-        <ul className="suggestions">
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
