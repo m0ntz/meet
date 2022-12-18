508eab0444e504ae386d0c8aca66d14a22ee2dc9
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-18 19:33:47 +0100

    Added beforeAll & afterAll
    Also fixed slowMo by placing first ignoreDefaultArgs

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,44 +1,42 @@
 import puppeteer from "puppeteer";
 
 describe("show/hide an event details", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(900000);
+    browser = await puppeteer.launch({
+      headless: false,
+      ignoreDefaultArgs: ["--disable-extensions"],
+      slowMo: 250,
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
   //Scenario 1:
   test("An event element is collapsed by default", async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
-
-    await page.waitForSelector(".event");
-
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 
   //Scenario 2
   test("User can expand an event to see its details", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
-
-    await page.waitForSelector(".event");
     await page.click(".event .details-btn");
 
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeDefined();
-    browser.close();
   });
 
   //Scenario 3
   test("User can collapse an event to hide its details", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/");
-
-    await page.waitForSelector(".event");
     await page.click(".event .details-btn");
     const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 });
