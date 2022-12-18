c43267f177dc9562d0691f2ef2b032683cc93d9b
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-18 02:04:40 +0100

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -10,9 +10,11 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    // this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
+
     this.setState({
       query: value,
       suggestions,
