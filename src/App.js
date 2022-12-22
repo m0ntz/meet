9fd27d430f36248262a6e58252e5fda17e299b69
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-22 22:10:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,14 +48,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {!navigator.onLine && (
+          <WarningAlert text="You are currently offline. The event list may not be up-to-date." />
+        )}
         <h1>Meet App</h1>
         <h2>Find events in your city</h2>
-        {!navigator.onLine && (
-          <WarningAlert
-            text="You are currently offline. The event list may not be up-to-date."
-            className="OfflineAlert"
-          />
-        )}
+
         <div className="container">
           <CitySearch
             locations={this.state.locations}
