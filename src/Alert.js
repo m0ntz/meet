73eb5069cea292d1af1d86bb6fdcc3f72c157553
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-22 21:23:11 +0100

    update alert

--- src/Alert.js
+++ src/Alert.js
@@ -38,7 +38,7 @@ class ErrorAlert extends Alert {
 class WarningAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = "orange";
+    this.color = "gray";
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -50,18 +50,20 @@ class App extends Component {
       <div className="App">
         <h1>Meet App</h1>
         <h2>Find events in your city</h2>
-        {!navigator.onLine && (
-          <WarningAlert text="You are currently offline. The event list may not be up-to-date." />
-        )}
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
-        <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
-          updateEvents={this.updateEvents}
-        />
-        <EventList events={this.state.events} />
+        <div className="container">
+          {!navigator.onLine && (
+            <WarningAlert text="You are currently offline. The event list may not be up-to-date." />
+          )}
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
+          />
+          <NumberOfEvents
+            numberOfEvents={this.state.numberOfEvents}
+            updateEvents={this.updateEvents}
+          />
+          <EventList events={this.state.events} />
+        </div>
       </div>
     );
   }
