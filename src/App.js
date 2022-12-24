b0cc55a2d640bd7e40271629f13e538d87f99fb5
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-24 03:27:44 +0100

    downgrade to recharts 2.1.16
    Fix chart not displaying

--- src/App.js
+++ src/App.js
@@ -45,6 +45,18 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   updateEvents = (location, eventCount) => {
     const { numberOfEvents } = this.state;
     if (location === undefined) location = this.state.selectedLocation;
@@ -62,18 +74,6 @@ class App extends Component {
     });
   };
 
-  getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter(
-        (event) => event.location === location
-      ).length;
-      const city = location.split(", ").shift();
-      return { city, number };
-    });
-    return data;
-  };
-
   render() {
     if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
@@ -85,7 +85,6 @@ class App extends Component {
         <h1>Meet App</h1>
         <h2>Find events in your city</h2>
 
-        {/* <div className="container"> */}
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
@@ -94,10 +93,9 @@ class App extends Component {
           numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
         />
+
         <h4>Events in each city</h4>
         <ScatterChart
-          width={400}
-          height={400}
           margin={{
             top: 20,
             right: 20,
@@ -106,18 +104,22 @@ class App extends Component {
           }}
         >
           <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" />
+          <XAxis type="category" dataKey="city" name="City" />
           <YAxis
             type="number"
             dataKey="number"
-            name="number of events"
+            name="Number of events"
             allowDecimals={false}
           />
           <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Scatter data={this.getData()} fill="#8884d8" />
+          <Scatter
+            data={this.getData()}
+            fill="#c97484"
+            name="Number of events in each city"
+          />
         </ScatterChart>
+
         <EventList events={this.state.events} />
-        {/* </div> */}
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
