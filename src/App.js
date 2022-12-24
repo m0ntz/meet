adec5051373a553b822e8e1005630026d55f6a30
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-24 21:46:30 +0100

    optimizations to charts

--- src/App.js
+++ src/App.js
@@ -97,9 +97,7 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <div className="data-vis-wrapper">
-          <h3>Event Topics</h3>
           <EventGenre events={this.state.events} />
-          <h3>City Events</h3>
           <ResponsiveContainer height={400}>
             <ScatterChart
               margin={{
