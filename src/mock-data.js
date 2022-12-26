62321503ecdc6a7f4df86817b4dfb35d21816329
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-12-26 01:49:43 +0100

    testing long titles

--- src/mock-data.js
+++ src/mock-data.js
@@ -172,7 +172,7 @@ const mockData = [
       "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MjNUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
     created: "2020-05-19T19:17:46.000Z",
     updated: "2020-05-27T12:01:32.356Z",
-    summary: "Learn JavaScript",
+    summary: "Intro to AngularJS-Remote",
     description:
       "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
     location: "London, UK",
